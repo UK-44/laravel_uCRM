521b3cacff1c2c740f4eff7ec82639536017aeba
UK-44 <cos.of.you.winds@gmail.com>
2025-02-15 00:43:34 +0900

    ssh接続設定追加

--- docker/production/entrypoint.sh
+++ docker/production/entrypoint.sh
@@ -4,5 +4,11 @@ set -e
 # PHP-FPM をバックグラウンドで起動
 php-fpm &
 
+# SSH サービスを起動
+service ssh start
+
+# Gunicorn をバックグラウンドで起動
+gunicorn -w 4 -b 0.0.0.0:8000 app:app &
+
 # Nginx をフォアグラウンドで起動（このプロセスが PID 1 となる）
 exec nginx -g "daemon off;"
